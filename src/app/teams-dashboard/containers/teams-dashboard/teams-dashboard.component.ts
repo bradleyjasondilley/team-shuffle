0c9171ca48596d89f1450299f09d5fd4b0c318bc
Solbrad <solbrad@gmail.com>
2018-12-10 07:26:38 +1300

    added roles

--- src/app/teams-dashboard/containers/teams-dashboard/teams-dashboard.component.ts
+++ src/app/teams-dashboard/containers/teams-dashboard/teams-dashboard.component.ts
@@ -17,22 +17,28 @@ export class TeamsDashboardComponent implements OnInit {
   }
 
   ngOnInit(){
-    this.teamsDashboradService.getUsers()
-      .subscribe((data: User[]) => {
+    // this.teamsDashboradService.getUsers()
+    //   .subscribe((data: User[]) => {
+    //     this.parseData(data);
+    //   });
+
+    this.teamsDashboradService.getData()
+      .subscribe((data: any) => {
+        console.log("data",data);
         this.parseData(data);
       });
   }
 
   parseData(data){
-    this.users = data;
+    this.users = data.users;
     let uniqueTeams: Object = new Object();
-    data.forEach(element => {
+    data.users.forEach(element => {
       uniqueTeams[element.pool] = 1;
     });
 
     Object.keys(uniqueTeams).sort().forEach(team=>{
       let members: Array<any> = [];
-      members = data.filter((member)=> member.pool === team);
+      members = data.users.filter((member)=> member.pool === team);
       this.teams.push({ "poolName": team, "members": members});
     });
 
--- src/app/teams-dashboard/teams-dashboard.service.ts
+++ src/app/teams-dashboard/teams-dashboard.service.ts
@@ -5,10 +5,12 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/forkJoin';
 
 import { User } from './models/user.interface';
 
 const USER_API: string = '/assets/data/users.json';
+const ROLES_API: string = '/assets/data/roles.json';
 
 @Injectable()
 export class TeamsDashboardService {
@@ -20,5 +22,24 @@ export class TeamsDashboardService {
           .map((response: Response) => response.json())
           .catch((error: any) => Observable.throw(error.json()));
     }
+
+    getRoles(): Observable<User[]> {
+        return this.http
+          .get(ROLES_API)
+          .map((response: Response) => response.json())
+          .catch((error: any) => Observable.throw(error.json()));
+    }
+
+    getData(): Observable<any> {
+        return Observable.forkJoin([
+          this.http.get(USER_API).map(res => res.json()),
+          this.http.get(ROLES_API).map(res => res.json())
+        ])
+        .map((data: any[]) => {
+          let users: any = data[0];
+          let roles: any[] = data[1];
+          return {"users":users, "roles":roles};
+        });
+      }
 
 }
